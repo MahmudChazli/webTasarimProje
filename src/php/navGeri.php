c66a57011ae04ab2874db6055985dc5ed2ac7b79
mahmudCh <mahmudch12@gmail.com>
2026-05-05 16:10:01 +0300

    rearranged and changed colors of cv nav and index btn bg

--- src/php/navGeri.php
+++ src/php/navGeri.php
@@ -1,6 +1,6 @@
 <nav class="nav-css fixed top-0">
-  <div style="padding: 5px 15px; padding-top: 10px;">
-    <a class="cursor-pointer hover:opacity-90" href="./index.php">&#8592;Geri</a>
+  <div style="padding: 5px 15px; padding-top: 5px;">
+    <a class="cursor-pointer hover:opacity-70" href="./index.php">&#8592;Geri</a>
   </div>
   <div class="nav-bottom-line-css"></div>
 </nav>
